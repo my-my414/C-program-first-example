f215456939c3a79b88db44942ff88634c6d6dac5
Akshaya H <81792372+my-my414@users.noreply.github.com>
2021-08-28 18:00:38 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+// skelton of c program 
+#include<stdio.h>//is the  header file that contains some standard function  and rules sets
+int main()//this function which will be called at first  when the program will be excecuted
+{
+    printf("AKSHAYA");
+    return 0;
+    
+}
